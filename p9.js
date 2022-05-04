3a720d8f7fb0d838844bf3ea1c199ca1f2327552
ravirathhod <104835863+ravirathhod@users.noreply.github.com>
2022-05-04 17:06:42 +0530

    Add files via upload

---
+++ p9.js
@@ -0,0 +1,22 @@
+var square
+function setup() {
+  createCanvas(400,400);
+  square=createSprite(200,200,50,50)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+if (keyIsDown(RIGHT_ARROW)){
+background("red")}
+if(keyIsDown(LEFT_ARROW)){
+  background("yellow")
+if(keyIsDown(LEFT_ARROW)){}
+}
+}
+
+
+
+
+
